729cd7909c7121987c038aa722f3539da9244523
reiddedeaux <rdedeaux@usf.edu>
2025-01-26 18:08:53 -0500

    Task 2 - Student Scores

--- CC_2.js
+++ CC_2.js
@@ -1,6 +1,21 @@
 // Task 1
 let products = ["Basketball", "Football","Baseball", "Tennis Ball", "Golf Ball"];
+
 products.push("Pickleball"); 
+
 console.log(products);
+
 products.pop();
+
 console.log("New Products:",products)
+
+//Task 2
+let scores = [95, 90, 55, 70, 100]; 
+scores[3] = 80;
+
+let total = scores.reduce((a, b) => a + b, 0);
+let average = total / scores.length ;
+
+console.log("New Scores", scores);
+console.log(total);
+console.log("Average Score:",average);
